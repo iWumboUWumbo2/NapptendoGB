a0f9b20d5efd40a31b7030309ebfe947f4401c70
John Connery <johnconnery@dhcp-vl2041-26232.eduroam.cornell.edu>
2021-10-24 23:55:50 -0400

    Added cmake

---
+++ cmake-build-debug/CMakeFiles/NapptendoGB.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/John Connery/CLionProjects/NapptendoGB/bus.c" "C:/Users/John Connery/CLionProjects/NapptendoGB/cmake-build-debug/CMakeFiles/NapptendoGB.dir/bus.c.obj"
+  "C:/Users/John Connery/CLionProjects/NapptendoGB/controls.c" "C:/Users/John Connery/CLionProjects/NapptendoGB/cmake-build-debug/CMakeFiles/NapptendoGB.dir/controls.c.obj"
+  "C:/Users/John Connery/CLionProjects/NapptendoGB/cpu.c" "C:/Users/John Connery/CLionProjects/NapptendoGB/cmake-build-debug/CMakeFiles/NapptendoGB.dir/cpu.c.obj"
+  "C:/Users/John Connery/CLionProjects/NapptendoGB/debug.c" "C:/Users/John Connery/CLionProjects/NapptendoGB/cmake-build-debug/CMakeFiles/NapptendoGB.dir/debug.c.obj"
+  "C:/Users/John Connery/CLionProjects/NapptendoGB/gpu.c" "C:/Users/John Connery/CLionProjects/NapptendoGB/cmake-build-debug/CMakeFiles/NapptendoGB.dir/gpu.c.obj"
+  "C:/Users/John Connery/CLionProjects/NapptendoGB/interrupts.c" "C:/Users/John Connery/CLionProjects/NapptendoGB/cmake-build-debug/CMakeFiles/NapptendoGB.dir/interrupts.c.obj"
+  "C:/Users/John Connery/CLionProjects/NapptendoGB/main.c" "C:/Users/John Connery/CLionProjects/NapptendoGB/cmake-build-debug/CMakeFiles/NapptendoGB.dir/main.c.obj"
+  "C:/Users/John Connery/CLionProjects/NapptendoGB/timers.c" "C:/Users/John Connery/CLionProjects/NapptendoGB/cmake-build-debug/CMakeFiles/NapptendoGB.dir/timers.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
